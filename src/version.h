66ce42c8b5cd3dc9e8763914bca9355f6d08d3be
Alexander Wenzel <Alexander.AW.Wenzel@bmw.de>
2011-10-20 15:33:41 +0200

    Updated release notes and version.

--- src/version.h
+++ src/version.h
@@ -1,7 +1,7 @@
 #ifndef __VERSION_H_
 #define __VERSION_H_
 
-#define PACKAGE_VERSION_STATE "Beta"
+#define PACKAGE_VERSION_STATE ""
 #define PACKAGE_VERSION "2.4.0"
 #define PACKAGE_REVISION ""
 #endif
