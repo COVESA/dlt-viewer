e50cd77021e6f888b79abf4dc10f73a1008d05da
Patrick Schlangen <patrick@schlangen.me>
2016-07-24 10:56:01 +0200

    Enable support for High DPI screens
    DLT Viewer sizes columns and other widgets based on an assumption
    of a "Low-DPI" screen resolution.
    
    On Windows systems with High DPI screens and enabled scaling,
    many GUI elements are too small and the application is very hard
    to use.
    
    This patch fixes the problem by enabling Qt's High DPI support
    introduced in Qt 5.6. To ensure the project still builds fine
    with Qt 5.5, the change has been wrapped into an #if testing
    the QT_VERSION.

--- src/main.cpp
+++ src/main.cpp
@@ -24,6 +24,11 @@
 
 int main(int argc, char *argv[])
 {
+#if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
+    QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
+#endif
+
     QApplication a(argc, argv);
 
     QStringList arguments = a.arguments();
