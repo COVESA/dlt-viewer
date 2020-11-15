f7878d7d264081af607fa4f891216fea01b9fd3f
Matthias Bilger <matthias@bilger.info>
2020-11-15 12:34:05 +0100

    Update dltuiutils.cpp

--- src/dltuiutils.cpp
+++ src/dltuiutils.cpp
@@ -13,8 +13,6 @@ QColor DltUiUtils::optimalTextColor(QColor forColor)
     // Counting the perceptive luminance - human eye favors green color...
     int a = ( 0.299 * forColor.red() + 0.587 * forColor.green() + 0.114 * forColor.blue());
 
-    qCritical() << forColor <<" " <<  a;
-
     if (a > 166)
     {
         d = 0; // bright colors - black font
