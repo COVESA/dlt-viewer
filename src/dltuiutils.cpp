7caf2a4f80b5da4e885a3304bbbe85589a198e77
Matthias Bilger <matthias@bilger.info>
2020-11-15 09:02:44 +0100

    Using better threshold for text color
    Improved the selection of the text color based on the background. Also
    fixed calculation (the real value has been used for red rather than the
    uint8 value).
    
    Signed-off-by: Matthias Bilger <matthias@bilger.info>

--- src/dltuiutils.cpp
+++ src/dltuiutils.cpp
@@ -11,9 +11,11 @@ QColor DltUiUtils::optimalTextColor(QColor forColor)
     int d = 0;
 
     // Counting the perceptive luminance - human eye favors green color...
-    double a = 1 - ( 0.299 * forColor.redF() + 0.587 * forColor.green() + 0.114 * forColor.blue())/255;
+    int a = ( 0.299 * forColor.red() + 0.587 * forColor.green() + 0.114 * forColor.blue());
 
-    if (a < 0.5)
+    qCritical() << forColor <<" " <<  a;
+
+    if (a > 166)
     {
         d = 0; // bright colors - black font
     }
