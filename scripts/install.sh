ba63f94311b9f18d085da0dc689887334eeac303
Hannes Achleitner <hannes.software@gmx.at>
2021-03-04 16:34:59 +0100

    Use qt 5 on macOS (#186)

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [[ "$(uname -s)" == 'Darwin' ]]; then
-    brew link qt --force
+    brew link qt@5 --force
 else
     sudo apt-get update
     sudo apt-get install -y build-essential qt5-default libqt5serialport5-dev
