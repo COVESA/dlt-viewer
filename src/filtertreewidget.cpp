89058c13396c85359252a39ec5505df8ed1cfd9b
jschmer-garmin <93265743+jschmer-garmin@users.noreply.github.com>
2021-11-23 13:36:00 +0100

    User experience enhancements for filters (#227)
    * add "Filter Clear all" option to Filter table context menu
    * support deleting all selected filters (multi-selection support)
    * support deleting all selected filters with the DELETE key
    
    Signed-off-by: Jens Schmer <jens.schmer@garmin.com>

--- src/filtertreewidget.cpp
+++ src/filtertreewidget.cpp
@@ -29,3 +29,38 @@ void FilterTreeWidget::dropEvent(QDropEvent *event)
          }
     }
 }
+
+void FilterTreeWidget::deleteSelected()
+{
+    // delete selected items
+    // and select the first item that replaces the first deleted item
+
+    QList<QTreeWidgetItem *> list = selectedItems();
+    if (list.size() == 0)
+        return;
+
+    auto select_idx = this->indexOfTopLevelItem(list.at(0));
+    for (auto& item : list) {
+        auto idx = indexOfTopLevelItem(item);
+        delete takeTopLevelItem(idx);
+    }
+
+    // select the item that replaced the first deleted item,
+    // but handle the case where all following items were removed, in
+    // that case select the last item available
+    if (select_idx >= topLevelItemCount())
+        select_idx = topLevelItemCount() - 1;
+
+    setCurrentItem(topLevelItem(select_idx));
+
+    emit filterCountChanged();
+}
+
+void FilterTreeWidget::keyReleaseEvent(QKeyEvent *event)
+{
+    // support some hotkeys in the filter list
+    if (event->key() == Qt::Key::Key_Delete)
+    {
+        deleteSelected();
+    }
+}
