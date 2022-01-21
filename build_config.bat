446d6588f20956347d4ae88a2cae300365c8f30d
Alexander Wenzel <alex@wenzelheim.de>
2022-01-21 09:19:39 +0100

    Update to Qt 5.15.2, Visual Studio 2019 Build Tools and Qwt 6.1.6 Prepare Release 2.21.4
    Signed-off-by: Alexander Wenzel <Alexander.AW.Wenzel@bmw.de>

--- build_config.bat
+++ build_config.bat
@@ -10,19 +10,15 @@ REM 5.15.2 msvc2015,msvc2019
 REM 6.2.2 msvc2019
 
 IF "%QTVER%"=="" (
-    set QTVER=5.12.12
-REM    set QTVER=5.15.2
+REM    set QTVER=5.12.12
+    set QTVER=5.15.2
 REM    set QTVER=6.2.2
 )
 
 IF "%MSVC_VER%"=="" (
-	if exist "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliary\Build" (
-     set MSVC_VER=2017
-REM	if exist "C:\Program Files (x86)\Microsoft Visual Studio\2019\BuildTools\VC\Auxiliary\Build" (
-REM		set MSVC_VER=2019
-	) else (
-		set MSVC_VER=2015
-	)
+REM	   set MSVC_VER=2015
+REM    set MSVC_VER=2017
+	set MSVC_VER=2019
 )
 
 echo Set QT directory for %QTVER% and %MSVC_VER%
@@ -34,6 +30,12 @@ if "%MSVC_VER%"=="2015" (
 	if "%MSVC_DIR%"=="" set "MSVC_DIR=C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC"
 ) else (
 	if "%MSVC_DIR%"=="" set "MSVC_DIR=C:\Program Files (x86)\Microsoft Visual Studio\%MSVC_VER%\BuildTools\VC\Auxiliary\Build"
+)
+
+IF "%QWT%"=="" (
+REM    set QWT=6.1.4
+    set QWT=6.1.6
+REM    set QWT=6.2.0
 )
 
 set PATH=%QTDIR%\bin;%MSVC_DIR%;%PATH%
