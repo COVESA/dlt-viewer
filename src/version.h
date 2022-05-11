f3f7c7dae6e61f72545b694f1182dc04fd32f65c
Alexander Wenzel <Alexander.AW.Wenzel@bmw.de>
2022-05-11 17:49:26 +0200

    Fix filter in DLT Test Robot Plugin
    Signed-off-by: Alexander Wenzel <Alexander.AW.Wenzel@bmw.de>

--- src/version.h
+++ src/version.h
@@ -23,11 +23,11 @@
 /* changing minor & major when layout of settings file config.ini changes */
 /* this kind of change is tracked in the settings dialogr */
 /* for other bugfixes and not major feature enhancement just use patch level */
-#define PACKAGE_VERSION_STATE "release"
+#define PACKAGE_VERSION_STATE "unstable"
 #define PACKAGE_MAJOR_VERSION 2
-#define PACKAGE_MINOR_VERSION 22
+#define PACKAGE_MINOR_VERSION 23
 #define PACKAGE_PATCH_LEVEL 0
-#define PACKAGE_VERSION "2.22.0"
+#define PACKAGE_VERSION "2.23.0"
 #define PACKAGE_REVISION ""
 #define PACKAGE_DESCRIPTION "DLT Viewer"
 #define DLT_SUPPORT_MAIL_ADDRESS "genivi-diagnostic-log-and-trace@lists.genivi.org"
