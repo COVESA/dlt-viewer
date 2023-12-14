4b891b7efbd60536d86ca6722205767ef6014616
Alexander Wenzel <Alexander.AW.Wenzel@bmw.de>
2023-12-14 09:54:26 +0100

    Prepare next unstable v2.26.0

--- src/version.h
+++ src/version.h
@@ -23,9 +23,9 @@
 /* changing minor & major when layout of settings file config.ini changes */
 /* this kind of change is tracked in the settings dialogr */
 /* for other bugfixes and not major feature enhancement just use patch level */
-#define PACKAGE_VERSION_STATE ""
+#define PACKAGE_VERSION_STATE "unstable"
 #define PACKAGE_MAJOR_VERSION 2
-#define PACKAGE_MINOR_VERSION 25
+#define PACKAGE_MINOR_VERSION 26
 #define PACKAGE_PATCH_LEVEL 0
 #define PACKAGE_VERSION "2.25.0"
 #define PACKAGE_REVISION "stable"
