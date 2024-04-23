40b7a11b3405c02cffc727dafb0df548716c1e72
Alexander Wenzel <Alexander.AW.Wenzel@bmw.de>
2024-04-23 14:32:59 +0200

    Update debian build package to focal. (#458)
    Signed-off-by: Alexander Wenzel <Alexander.AW.Wenzel@bmw.de>

--- build_viewer_debs.sh
+++ build_viewer_debs.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
-# building DLT Viewer debian packages on Ubuntu 18.04 / Bionic Beaver
+# building DLT Viewer debian packages on Ubuntu 20.04 / Focal Fossa
 # script created 19.6.2018, Gernot Wirschal
+# updated 23.04.2024, Alexander Wenzel
 set -e
 rm -rf debtmp
 DTLREV=`git rev-list --all --count`
-cp -r bionic/debian .
+cp -r focal/debian .
 sed -i '/#define PACKAGE_REVISION/c #define PACKAGE_REVISION "'$DTLREV'"' src/version.h
 DLT_VERSION=`cat src/version.h | grep "PACKAGE_VERSION" | grep -v PACKAGE_VERSION_STATE | awk '{print $3}' | tr -d \" `
-sed -i '/genivi-dlt-viewer/c genivi-dlt-viewer ('${DLT_VERSION}') stable; urgency=low' ./debian/changelog
+sed -i '/covesa-dlt-viewer/c covesa-dlt-viewer ('${DLT_VERSION}') stable; urgency=low' ./debian/changelog
 echo "DLT_VERSION: ${DLT_VERSION}, $DTLREV"
 
 mkdir -p ./debtmp/dlt-viewer-${DLT_VERSION}
@@ -15,10 +16,10 @@ rsync -a --exclude=debtmp --exclude=*.sh --exclude=*.bat --exclude=cache --exclu
 
 cd debtmp/dlt-viewer-${DLT_VERSION}
 echo "#############################################"
-echo "Create tarball ../genivi-dlt-viewer_${version}.orig.tar.gz"
+echo "Create tarball ../covesa-dlt-viewer_${DLT_VERSION}.orig.tar.gz"
 echo "#############################################"
 
-tar -czf ../genivi-dlt-viewer_${DLT_VERSION}.orig.tar.gz *
+tar -czf ../covesa-dlt-viewer_${DLT_VERSION}.orig.tar.gz *
 echo "y" | debuild -uc -us
 #make -j8 install
 
