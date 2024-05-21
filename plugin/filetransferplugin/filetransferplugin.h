34740dbcf65914932ea409abf65fb9f2ff50ce25
Alexander Wenzel <Alexander.AW.Wenzel@bmw.de>
2024-05-21 10:09:03 +0200

    Update Plugin version number.
    Signed-off-by: Alexander Wenzel <Alexander.AW.Wenzel@bmw.de>

--- plugin/filetransferplugin/filetransferplugin.h
+++ plugin/filetransferplugin/filetransferplugin.h
@@ -26,7 +26,7 @@
 #include "globals.h"
 #include "configuration.h"
 
-#define FILETRANSFER_PLUGIN_VERSION "1.4.1"
+#define FILETRANSFER_PLUGIN_VERSION "1.4.2"
 
 class FiletransferPlugin : public QObject, QDLTPluginInterface, QDltPluginViewerInterface, QDltPluginCommandInterface, QDltPluginControlInterface
 {
