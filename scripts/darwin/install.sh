627ba630804147045e63f628f9551176e19ad444
Hannes Achleitner <hannes.software@gmx.at>
2024-06-10 08:54:11 +0200

    follow brew install suggestions

--- scripts/darwin/install.sh
+++ scripts/darwin/install.sh
@@ -17,6 +17,13 @@ if [[ -z "$QT_VERSION" ]]; then
     exit 1
 fi
 
+# follow brew install suggestions
+echo 'export PATH="/opt/homebrew/opt/qt@5/bin:$PATH"' >> ~/.bash_profile
+export LDFLAGS="-L/opt/homebrew/opt/qt@5/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/qt@5/include"
+export PKG_CONFIG_PATH="/opt/homebrew/opt/qt@5/lib/pkgconfig"
+source ~/.bash_profile
+
 uname -a
 if [[ $(uname -m) == 'arm64' ]]; then
   echo "Run on MacOS arm64"
