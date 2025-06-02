1b5cc92c6a8c86d00347098e7841732a29ce130d
Renu-Priya411 <renupriya.ka.krishnamoorthy@partner.bmwgroup.com>
2025-06-02 10:00:16 +0530

    Dlt Counter Plugin : Used for finding the missing sequence of messages using the counter and the context ID values. The missing sequence of numbers will be visible in a new window. The data can be exported to local machine as well.
    Changes made in mainwindow.cpp, qdltcontrol and dltCounterPlugin
    
    Signed-off by : Renu Priya Krishnamoorthy <RenuPriya.KA.Krishnamoorthy@partner.bmwgroup.com>

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -4643,8 +4643,6 @@ void MainWindow::getModel()
         }
     }
 
-    // QStandardItemModel *missingDataModel = new QStandardItemModel();
-
     missingDataModel->setColumnCount(2);
     missingDataModel->setHorizontalHeaderLabels({"Ctid", "Missing Counter"});
 
@@ -4663,7 +4661,7 @@ void MainWindow::getModel()
             while (expectedValue < counter) {
 
                 // Add missing values to the model
-                QList<QStandardItem*> rowItems;
+\                QList<QStandardItem*> rowItems;
                 rowItems.append(new QStandardItem(ctid));
                 rowItems.append(new QStandardItem(QString::number(expectedValue)));
                 missingDataModel->appendRow(rowItems);
@@ -4672,8 +4670,6 @@ void MainWindow::getModel()
             }
             expectedValue = counter + 1;  // Move to the next expected value after the current counter
         }
-
-
         // Check for any missing values till 255
         while (expectedValue <= 255) {
             QList<QStandardItem*> rowItems;
@@ -8497,7 +8493,6 @@ void MainWindow::exportCounterData()
         QString ctid = model->data(model->index(row, 6)).toString();
         // QString ctid = model->item(row, 1)->text();
         QString ctid = model->data(model->index(row, 6)).toString();
-        // int counter = model->item(row, 0)->text().toInt();
         int counter = model->data(model->index(row, 3)).toInt();
 
         ctidCounterMap[ctid].insert(counter);
@@ -8527,7 +8522,6 @@ void MainWindow::exportCounterData()
         int expectedValue = 1;
 
         for (int counter : counters) {
-            // If expectedValue is less than the current counter, those values are missing
             while (expectedValue < counter) {
                 // Add missing values to the model
                 QList<QStandardItem*> rowItems;
@@ -8567,6 +8561,7 @@ void MainWindow::exportCounterData()
     for (int row = 0; row < missingDataModel->rowCount(); ++row) {
         QString ctid = missingDataModel->item(row, 0)->text();
         QString missingCounter = missingDataModel->item(row, 1)->text();
+
         // Write data in CSV format
         stream << ctid << "," << missingCounter << "\n";
     }
