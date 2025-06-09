COVESA/dlt-viewer

commit a7bd8af1fffc9711e535704183853bedbd8e1c14

populate ecus tree efficiently
fix symbols visibility

Fix macos-13 build

It still uses qmake, so new sources should be added there too

Signed-off-by: Viktor Kopp <vifactor@gmail.com>

src/mainwindow.cpp
@@ -84,6 +84,7 @@ extern "C" {
 #include "sortfilterproxymodel.h"
 #include "qdltoptmanager.h"
 #include "qdltctrlmsg.h"
+#include "ecutree.h"
 
 
 MainWindow::MainWindow(QWidget *parent) :
@@ -1889,45 +1890,52 @@ void MainWindow::on_action_menuFile_Clear_triggered()
     return;
 }
 
-void MainWindow::contextLoadingFile(const QDltMsg &msg)
+void MainWindow::populateEcusTree(EcuTree&& ecuTree)
 {
-    /* find ecu item */
-    EcuItem *ecuitemFound = 0;
-    for(int num = 0; num < project.ecu->topLevelItemCount (); num++)
-    {
-        EcuItem *ecuitem = (EcuItem*)project.ecu->topLevelItem(num);
-        if(ecuitem->id == msg.getEcuid())
-        {
-            ecuitemFound = ecuitem;
-            break;
+    QList<QTreeWidgetItem*> ecus;
+    // populate ECUs tree view
+    for (auto& [ecuid, apps] : ecuTree.ecus) {
+        EcuItem* ecuItem = new EcuItem(nullptr);
+
+        ecuItem->id = ecuid;
+
+        QList<QTreeWidgetItem*> appsItems;
+        for(const auto& [appid, appdata] : apps) {
+            ApplicationItem* appItem = new ApplicationItem(ecuItem);
+            appItem->id = appid;
+            appItem->description = appdata.description;
+            appItem->update();
+
+            QList<QTreeWidgetItem*> contextsItems;
+            for(const auto& [ctxid, ctxdata] : appdata.contexts) {
+                ContextItem* conItem = new ContextItem(appItem);
+                conItem->id = ctxid;
+                conItem->loglevel = ctxdata.logLevel;
+                conItem->tracestatus = ctxdata.traceStatus;
+                conItem->description = ctxdata.description;
+                conItem->status = ContextItem::valid;
+                conItem->update();
+
+                contextsItems.append(conItem);
+            }
+
+            appItem->addChildren(contextsItems);
+            appsItems.append(appItem);
         }
+
+        ecuItem->addChildren(appsItems);
+        ecuItem->update();
+
+        pluginManager.stateChanged(ecus.size(), QDltConnection::QDltConnectionOffline,
+                                   ecuItem->getHostname());
+
+        ecus.append(ecuItem);
     }
 
-    if(!ecuitemFound)
-    {
-        /* no Ecuitem found, create a new one */
-        ecuitemFound = new EcuItem(0);
+    project.ecu->addTopLevelItems(ecus);
 
-        /* update ECU item */
-        ecuitemFound->id = msg.getEcuid();
-        ecuitemFound->update();
-
-        /* add ECU to configuration */
-        project.ecu->addTopLevelItem(ecuitemFound);
-
-        /* Update the ECU list in control plugins */
-        updatePluginsECUList();
-
-        pluginManager.stateChanged(project.ecu->indexOfTopLevelItem(ecuitemFound), QDltConnection::QDltConnectionOffline,ecuitemFound->getHostname());
-
-    }
-
-    controlMessage_ReceiveControlMessage(ecuitemFound, msg);
-}
-
-void MainWindow::reloadLogFileStop()
-{
-
+    /* Update the ECU list in control plugins */
+    updatePluginsECUList();
 }
 
 void MainWindow::reloadLogFileProgressMax(int num)
@@ -2058,13 +2066,21 @@ void MainWindow::reloadLogFileFinishFilter()
             settings->updateContextLoadingFile) {
         const QList<int> &msgIndexList = dltIndexer->getGetLogInfoList();
 
-        // FIXME: this is slow operation running in the main loop
         QDltMsg msg;
+        EcuTree ecuTree;
         for (const auto msgIndex : msgIndexList) {
             if (qfile.getMsg(msgIndex, msg)) {
-                contextLoadingFile(msg);
+                auto ctrlMsg = qdlt::msg::payload::parse(msg.getPayload(), msg.getEndianness() == QDlt::DltEndiannessBigEndian);
+                std::visit([&ecuTree, ecuId = msg.getEcuid()](auto&& payload) {
+                    using T = std::decay_t<decltype(payload)>;
+                    if constexpr (std::is_same_v<T, qdlt::msg::payload::GetLogInfo>) {
+                        ecuTree.add(ecuId, payload);
+                    }
+                }, ctrlMsg);
             }
         }
+        project.ecu->clear();
+        populateEcusTree(std::move(ecuTree));
     }
 
     // reconnect ecus again

qdlt/qdlt.pro
@@ -68,6 +68,7 @@ SOURCES +=  \
     fieldnames.cpp \
     qdltimporter.cpp \
     dltmessagematcher.cpp \
+    qdltctrlmsg.cpp \
 
 HEADERS += qdlt.h \
     export_rules.h \
@@ -101,6 +102,7 @@ HEADERS += qdlt.h \
     fieldnames.h \
     qdltimporter.h \
     dltmessagematcher.h \
+    qdltctrlmsg.h \
 
 unix:VERSION            = 1.0.0
 

qdlt/qdltctrlmsg.h
@@ -1,6 +1,8 @@
 #ifndef QDLTCTRLMSG_H
 #define QDLTCTRLMSG_H
 
+#include "export_rules.h"
+
 #include <vector>
 #include <variant>
 
@@ -57,7 +59,7 @@ struct UnregisterContext {
 using Type = std::variant<GetLogInfo, GetSoftwareVersion, GetDefaultLogLevel, SetLogLevel, Timezone,
                           UnregisterContext>;
 
-Type parse(const QByteArray&, bool isBigEndian);
+QDLT_EXPORT Type parse(const QByteArray&, bool isBigEndian);
 
 } // namespace qdlt::msg::payload
 

src/CMakeLists.txt
@@ -51,7 +51,9 @@ add_executable(dlt-viewer
     searchform.cpp
     ${UI_RESOURCES_RCC}
     resources/dlt_viewer.rc
-    )
+    ecutree.h
+    ecutree.cpp
+)
 
 target_link_libraries(dlt-viewer
     qdlt

src/ecutree.cpp
@@ -1,0 +1,17 @@
+#include "ecutree.h"
+
+void EcuTree::add(const QString& ecuId, const qdlt::msg::payload::GetLogInfo& info)
+{
+    for (const auto& app : info.apps) {
+        App appData;
+        appData.description = app.description;
+        for (const auto& ctx : app.ctxs) {
+            Ctx ctxData;
+            ctxData.description = ctx.description;
+            ctxData.logLevel = ctx.logLevel;
+            ctxData.traceStatus = ctx.traceStatus;
+            appData.contexts[ctx.id] = std::move(ctxData);
+        }
+        ecus[ecuId][app.id] = std::move(appData);
+    }
+}

src/ecutree.h
@@ -1,0 +1,27 @@
+#ifndef ECUTREEBUILDER_H
+#define ECUTREEBUILDER_H
+
+#include <qdltctrlmsg.h>
+
+#include <map>
+
+struct EcuTree
+{
+    void add(const QString& ecuId, const qdlt::msg::payload::GetLogInfo&);
+
+    using EcuId = QString;
+    using AppId = QString;
+    using CtxId = QString;
+    struct Ctx {
+        QString description;
+        int8_t logLevel;
+        int8_t traceStatus;
+    };
+    struct App {
+        QString description;
+        std::map<CtxId, Ctx> contexts;
+    };
+    std::map<EcuId, std::map<AppId, App>> ecus;
+};
+
+#endif // ECUTREEBUILDER_H

src/mainwindow.h
@@ -96,6 +96,8 @@ namespace Ui {
     class MainWindow;
 }
 
+struct EcuTree;
+
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
@@ -236,13 +238,8 @@ private:
 
     void getSelectedItems(EcuItem **ecuitem,ApplicationItem** appitem,ContextItem** conitem);
 
-    /**
-        * @brief Reload the complete log file
-        * @param update if this parameter is false, the file is loaded the first time, if true the reload is performed because of a changed configuration
-        *
-        */
-    void reloadLogFileStop();
     void reloadLogFile(bool update=false, bool multithreaded = true);
+    void populateEcusTree(EcuTree&& ecuTree);
 
     void reloadLogFileDefaultFilter();
 
@@ -275,7 +272,6 @@ private:
     void updatePluginsECUList();
     void updatePlugins();
     void updatePlugin(PluginItem *item);
-    void contextLoadingFile(const QDltMsg &msg);
     void versionString(const QDltMsg &msg);
     void pluginsAutoload(QString version);
 

src/src.pro
@@ -149,6 +149,7 @@ SOURCES += main.cpp \
     dltmsgqueue.cpp \
     dltfileindexerthread.cpp \
     dltfileindexerdefaultfilterthread.cpp \
+    ecutree.cpp \
 
 # Show these headers in the project
 HEADERS += mainwindow.h \
@@ -180,7 +181,8 @@ HEADERS += mainwindow.h \
     dltmsgqueue.h \
     dltfileindexerthread.h \
     dltfileindexerdefaultfilterthread.h \
-    mcudpsocket.h
+    mcudpsocket.h \
+    ecutree.h \
 
 # Compile these UI files
 FORMS += mainwindow.ui \

commit f500efd5f39eb6689fc79bec360fcf8372599147

Fix ECUs config tree when importing dlp
This is a fix of regression introduced with
https://github.com/COVESA/dlt-viewer/commit/a7bd8af1fffc9711e535704183853bedbd8e1c14

The fix prevents running the indexer when importing dlp,
otherwise indexer calls reloadLogFileFinishFilter which
recreates empty ecu tree

https://github.com/COVESA/dlt-viewer/issues/667

src/mainwindow.cpp
@@ -2544,9 +2544,6 @@ bool MainWindow::openDlpFile(QString fileName)
         if(QDltOptManager::getInstance()->isCommandlineMode())
             // if dlt viewer started as converter or with plugin option load file non multithreaded
             reloadLogFile(false,false);
-        else
-            // normally load log file mutithreaded
-            reloadLogFile();
         return true;
     } else {
         return false;