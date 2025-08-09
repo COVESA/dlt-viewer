6bc26166ce82f7391f9f44742907000200bcff91
Viktor Kopp <vifactor@gmail.com>
2025-08-09 23:26:33 +0200

    Use custom promoted FileExplorer class for fs view
    Add FileExplorerTab widget for fs view
    
    It encapsulate file handling operations
    
    Signed-off-by: Viktor Kopp <vifactor@gmail.com>

---
+++ src/fileexplorertab.h
@@ -0,0 +1,45 @@
+#ifndef FILEEXPLORERTAB_H
+#define FILEEXPLORERTAB_H
+
+#include "sortfilterproxymodel.h"
+
+#include <QFileSystemModel>
+#include <QWidget>
+
+namespace Ui {
+class FileExplorerTab;
+}
+
+class FileExplorerTab : public QWidget {
+    Q_OBJECT
+
+  public:
+    explicit FileExplorerTab(QWidget* parent = nullptr);
+    ~FileExplorerTab() override;
+
+    void setCurrentFile(const QString& path);
+
+    void setSortingOrder(Qt::SortOrder order);
+    void setSortingMode(SortFilterProxyModel::SortType type);
+
+  private:
+     QString getPathFromModelIndex(const QModelIndex &index) const;
+
+  signals:
+    void fileActivated(const QString& path);
+  private slots:
+    void on_exploreView_activated(const QModelIndex &index);
+    void on_exploreView_customContextMenuRequested(QPoint pos);
+    void on_comboBoxExplorerSortType_currentIndexChanged(int index);
+
+    void on_comboBoxExplorerSortOrder_currentIndexChanged(int index);
+
+
+  private:
+    Ui::FileExplorerTab* ui;
+
+    QFileSystemModel* m_fsModel;
+    SortFilterProxyModel * m_fsSortProxyModel;
+};
+
+#endif // FILEEXPLORERTAB_H
