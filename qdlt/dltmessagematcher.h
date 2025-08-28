6f93dff755a7261d182d13ebe2ac5395ec8e42f8
Michael Foukarakis <michael.foukarakis@bmw.de>
2025-08-28 13:14:44 +0200

    qdlt: Add some basic doc
    * Add some basic descriptions for the headers in qdlt/
      so that some relationships are clear in generated graphs.
    * Some basic docs in locations where code is parsing DLT message(s)
      or DLT messages are created based on internal state.
    
    This is not complete by far, most functions miss documentation
    and there are no statements of intent anywhere.
    
    Signed-off-by: Michael Foukarakis <michael.foukarakis@bmw.de>

--- qdlt/dltmessagematcher.h
+++ qdlt/dltmessagematcher.h
@@ -11,6 +11,17 @@
 
 class QDltMsg;
 
+/**
+ * A class which can be used to set search parameters for matching on potential DLT messages.
+ *
+ * Offers functions to search different parts of a DLT message.
+ * Offers the option to search either the header, the payload, or both.
+ * Searches can be case-sensitive - default is case-insensitive.
+ * The pattern to search a payload can be either a regular expression or plain text.
+ *
+ * Currently used by the UI search dialog.
+ * @see SearchDialog
+ */
 class QDLT_EXPORT DltMessageMatcher
 {
 public:
@@ -46,9 +57,32 @@ public:
         m_messageIdFormat = msgIdFormat;
     }
 
+    /**
+     *
+     * @return `true` if `msg` matches all of:
+     *  - application ID set in `this`
+     *  - context ID set in `this`
+     *  - timestamp range set in `this`
+     *  - pattern matches either the `msg` header or payload
+     *  `false` otherwise.
+     *
+     *  @see SearchDialog::findMessages
+     */
     bool match(const QDltMsg& message, const Pattern& pattern) const;
+
 private:
+
+    /**
+     * Match the application ID in `appId` against `this`
+     *
+     * @return `true` if `this` has no application ID set or the IDs match exactly, false otherwise
+     */
     bool matchAppId(const QString& appId) const;
+    /**
+     * Match the context ID in `ctxId` against `this`
+     *
+     * @return `true` if `this` has no application ID set or the IDs match exactly, false otherwise
+     */
     bool matchCtxId(const QString& ctxId) const;
     bool matchTimestampRange(unsigned int ts) const;
 private:
