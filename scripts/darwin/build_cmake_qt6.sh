67941004fa4f2981835aebf438524263cbb0ea73
bunty95 <146869867+bunty95@users.noreply.github.com>
2026-03-28 17:22:01 -0400

    update syntax

--- scripts/darwin/build_cmake_qt6.sh
+++ scripts/darwin/build_cmake_qt6.sh
@@ -14,9 +14,6 @@ cd "${BUILD_DIR}"
 Qt6_DIR="/usr/local/opt/qt"
 echo "Build with qmake $(uname -m) $Qt6_DIR"
 
-
-#make
-
 echo Cleanup
 rm -rf "${INSTALL_DIR}"
 rm -rf "${SRC_DIR}/build"
@@ -28,7 +25,7 @@ echo Build with CMake
 # https://developer.apple.com/library/archive/documentation/CoreFoundation/Conceptual/CFBundles/BundleTypes/BundleTypes.html
 cmake -G Ninja \
   -DCMAKE_INSTALL_PREFIX=${INSTALL_DIR} \
-  -DCMAKE_PREFIX_PATH=${Qt5_DIR}/lib/cmake \
+  -DCMAKE_PREFIX_PATH=${Qt6_DIR}/lib/cmake \
   -DCMAKE_OSX_DEPLOYMENT_TARGET=12.0 \
   -DCMAKE_BUILD_TYPE=Release \
   -DDLT_USE_QT_RPATH=ON \
@@ -48,7 +45,7 @@ ls -l build/install/DLTViewer.app
 otool -L build/install/DLTViewer.app/Contents/MacOS/dlt-viewer
 otool -l build/install/DLTViewer.app/Contents/MacOS/dlt-viewer | grep -A2 LC_RPATH
 echo "Deploying DLTViewer.app with macdeployqt"
-    ${QT_ROOT_DIR}"bin/macdeployqt" build/install/DLTViewer.app -verbose=1 -no-codesign \
+${Qt6_DIR}"bin/macdeployqt" build/install/DLTViewer.app -verbose=1 -no-codesign \
   -libpath=$(pwd)/build/install/DLTViewer.app/Contents/Frameworks \
   -executable=$(pwd)/build/install/DLTViewer.app/Contents/MacOS/dlt-viewer
 
