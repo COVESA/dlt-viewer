1ccbc83e807ac14bc586503d0fcc100f0fad0a74
bunty95 <146869867+bunty95@users.noreply.github.com>
2026-04-01 14:05:24 -0400

    Fix architecture detection in build script

--- build_sdk_windows_qt6_MSVC_cmake.bat
+++ build_sdk_windows_qt6_MSVC_cmake.bat
@@ -1,5 +1,4 @@
 REM @echo off
-set ARCH=%PROCESSOR_ARCHITECTURE%
 
 REM Date     Version   Author                Changes
 REM 4.7.19   1.0       Alexander Wenzel      Update to Qt 5.12.4 and Visual Studio 2015
@@ -59,6 +58,7 @@ echo ************************************
 echo ************************************
 echo ***  Configure MSVC environment  ***
 echo ************************************
+set ARCH=%PROCESSOR_ARCHITECTURE%
 
 if /i "%ARCH%"=="AMD64" (
 echo [RESULT] Architecture: x64 (Intel/AMD)
@@ -102,11 +102,8 @@ if %ERRORLEVEL% NEQ 0 GOTO ERROR_HANDLER
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DDLT_PARSER=ON -DCMAKE_INSTALL_PREFIX=%DLT_VIEWER_SDK_DIR% ..\..
 if %ERRORLEVEL% NEQ 0 GOTO ERROR_HANDLER
 
-if /i "%ARCH%"=="AMD64" (
-echo [RESULT] Architecture: x64 (Intel/AMD)
 for /f "skip=2 tokens=2 delims== " %%C in ('wmic cpu get NumberOfLogicalProcessors /value') do set /a PARALLEL=%%C-1
 if %PARALLEL% LSS 1 set PARALLEL=1
-)
 
 echo * PARALLEL = %PARALLEL%
 
