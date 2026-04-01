dcaf32285e3e813faa9b74b90d252d0d27e0d8f3
bunty95 <146869867+bunty95@users.noreply.github.com>
2026-04-01 14:24:46 -0400

    revert changes
    Removed ARM64 architecture handling and related echo statements from the script.

--- build_sdk_windows_qt6_MSVC_cmake.bat
+++ build_sdk_windows_qt6_MSVC_cmake.bat
@@ -58,21 +58,10 @@ echo ************************************
 echo ************************************
 echo ***  Configure MSVC environment  ***
 echo ************************************
-set ARCH=%PROCESSOR_ARCHITECTURE%
 
-if /i "%ARCH%"=="AMD64" (
-echo [RESULT] Architecture: x64 (Intel/AMD)
 call vcvarsall.bat x86_amd64
 if %ERRORLEVEL% NEQ 0 goto ERROR_HANDLER
 echo configuring was successful
-)
-
-if /i "%ARCH%"=="ARM64" (
-echo [RESULT] Architecture: ARM64 (Snapdragon/Qualcomm)
-call vcvarsall.bat arm64
-if %ERRORLEVEL% NEQ 0 goto ERROR_HANDLER
-echo configuring was successful
-)
 
 if exist %DLT_VIEWER_SDK_DIR% (
 echo ************************************
@@ -102,8 +91,6 @@ if %ERRORLEVEL% NEQ 0 GOTO ERROR_HANDLER
 cmake -G Ninja -DCMAKE_BUILD_TYPE=Release -DDLT_PARSER=ON -DCMAKE_INSTALL_PREFIX=%DLT_VIEWER_SDK_DIR% ..\..
 if %ERRORLEVEL% NEQ 0 GOTO ERROR_HANDLER
 
-if /i "%ARCH%"=="AMD64" (
-echo [RESULT] Architecture: x64 (Intel/AMD)
 for /f "skip=2 tokens=2 delims== " %%C in ('wmic cpu get NumberOfLogicalProcessors /value') do set /a PARALLEL=%%C-1
 if %PARALLEL% LSS 1 set PARALLEL=1
 
@@ -111,15 +98,6 @@ echo * PARALLEL = %PARALLEL%
 
 cmake --build . --parallel %PARALLEL%
 if %ERRORLEVEL% NEQ 0 GOTO ERROR_HANDLER
-)
-
-if /i "%ARCH%"=="ARM64" (
-echo [RESULT] Architecture: ARM64 (Snapdragon/Qualcomm)
-cmake --build . 
-if %ERRORLEVEL% NEQ 0 GOTO ERROR_HANDLER
-)
-
-
 
 echo ************************************
 echo ***         Create SDK           ***
